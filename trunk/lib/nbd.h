4eca95696a597baff452d3f81ad67ac2f21634fc
Takahiro Hirofuchi <t.hirofuchi@aist.go.jp>
2011-02-22 18:02:22 +0900

    1. add license notes to each sources. 2. remove the source code derived from the original NBD server.

--- trunk/lib/nbd.h
+++ trunk/lib/nbd.h
@@ -1,8 +1,26 @@
 /* 
- * Copyright (C) 2008-2010 National Institute of Advanced Industrial Science and Technology
+ * xNBD - an enhanced Network Block Device program
  *
- * Author: Takahiro Hirofuchi
+ * Copyright (C) 2008-2011 National Institute of Advanced Industrial Science
+ * and Technology
+ *
+ * Author: Takahiro Hirofuchi <t.hirofuchi _at_ aist.go.jp>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
+
 #ifndef LIB_XNBD_NBD_H
 #define LIB_XNBD_NBD_H
 
@@ -19,57 +37,24 @@
 #include <sys/stat.h>
 
 
-/* ------------------------------------------------------------------------------------------ */
-/* ------------------------------------------------------------------------------------------ */
+/* Be compatible with the NBD protocol. */
 
-/* this part comes from nbd-2.9.11 */
-
-/* excerpt from the original nbd.h */
-/*
- * 1999 Copyright (C) Pavel Machek, pavel@ucw.cz. This code is GPL.
- * 1999/11/04 Copyright (C) 1999 VMware, Inc. (Regis "HPReg" Duchesne)
- *            Made nbd_end_request() use the io_request_lock
- * 2001 Copyright (C) Steven Whitehouse
- *            New nbd_end_request() for compatibility with new linux block
- *            layer code.
- * 2003/06/24 Louis D. Langholtz <ldl@aros.net>
- *            Removed unneeded blksize_bits field from nbd_device struct.
- *            Cleanup PARANOIA usage & code.
- * 2004/02/19 Paul Clements
- *            Removed PARANOIA, plus various cleanup and comments
- */
 #define NBD_REQUEST_MAGIC 0x25609513
-#define NBD_REPLY_MAGIC 0x67446698
+#define NBD_REPLY_MAGIC   0x67446698
 
-/*
- * This is the packet used for communication between client and
- * server. All data are in network byte order.
- */
 struct nbd_request {
 	uint32_t magic;
-	uint32_t type;       /* == READ || == WRITE  */
-	char handle[8];
+	uint32_t type;
+	uint64_t handle;
 	uint64_t from;
 	uint32_t len;
-} __attribute__((packed));
-
-/*
- * This is the reply packet that nbd-server sends back to the client after
- * it has completed an I/O request (or an error occurs).
- */
-struct nbd_reply {
-	uint32_t magic;
-	uint32_t error;              /* 0 = ok, else error   */
-	char handle[8];         /* handle you got from request  */
 };
 
-
-/* end excerpt from the original nbd */
-
-/* ------------------------------------------------------------------------------------------ */
-/* ------------------------------------------------------------------------------------------ */
-
-
+struct nbd_reply {
+	uint32_t magic;
+	uint32_t error;
+	uint64_t handle;
+};
 
 enum {
 	NBD_CMD_READ = 0,
@@ -88,12 +73,8 @@ int nbd_negotiate_with_client_readonly(int sockfd, off_t exportsize);
 off_t nbd_negotiate_with_server(int sockfd);
 int nbd_negotiate_with_server2(int sockfd, off_t *exportsize, uint32_t *exportflags);
 
-#define INIT_PASSWD "NBDMAGIC"
-
-/* Flags used between the client and server */
-#define NBD_FLAG_HAS_FLAGS      (1 << 0)        /* Flags are there */
-#define NBD_FLAG_READ_ONLY      (1 << 1)        /* Device is read-only */
-
+#define NBD_FLAG_HAS_FLAGS      (1 << 0)
+#define NBD_FLAG_READ_ONLY      (1 << 1)
 
 
 
--- trunk/xnbd_tester.c
+++ trunk/xnbd_tester.c
@@ -1,5 +1,24 @@
 /* 
- * Copyright (C) 2008-2010 National Institute of Advanced Industrial Science and Technology
+ * xNBD - an enhanced Network Block Device program
+ *
+ * Copyright (C) 2008-2011 National Institute of Advanced Industrial Science
+ * and Technology
+ *
+ * Author: Takahiro Hirofuchi <t.hirofuchi _at_ aist.go.jp>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 #include "xnbd.h"
@@ -249,9 +268,7 @@ void recv_reply_header(int remotefd, uint64_t expected_index)
 	if (error)
 		err("reply state error %d", error);
 
-	uint64_t reply_index = 0;
-	memcpy(&reply_index, reply.handle, 8);
-
+	uint64_t reply_index = ntohll(reply.handle);
 
 	dbg("index %ju", reply_index);
 
