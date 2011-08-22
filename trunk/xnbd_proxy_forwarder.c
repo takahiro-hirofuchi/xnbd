0c441156e9e0c473e8dc5712b564668b014906ea
Takahiro Hirofuchi <t.hirofuchi@aist.go.jp>
2011-08-22 17:50:43 +0900

    fix over-4GB disk migration on a 32bit OS.

--- trunk/xnbd_proxy_forwarder.c
+++ trunk/xnbd_proxy_forwarder.c
@@ -262,9 +262,10 @@ void *forwarder_tx_thread_main(void *arg)
 
 		/* send read request as soon as possible */
 		for (int i = 0; i < priv->nreq; i++) {
+			off_t iofrom = (iofrom) priv->req[i].bindex_iofrom * CBLOCKSIZE;
 			size_t length = priv->req[i].bindex_iolen * CBLOCKSIZE;
 
-			int ret = nbd_client_send_read_request(proxy->remotefd, priv->req[i].bindex_iofrom * CBLOCKSIZE, length);
+			int ret = nbd_client_send_read_request(proxy->remotefd, iofrom, length);
 			if (ret < 0) {
 				warn("sending read request failed, seqnum %lu", priv->seqnum);
 				sending_failed = 1;
