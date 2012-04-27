db8470d567333ad831f2135954883f028c2c1026
hama5 <none@none>
2012-04-27 19:41:59 +0900

    Rework commit:1191efe8c37d

--- trunk/xnbd_proxy.c
+++ trunk/xnbd_proxy.c
@@ -224,9 +224,7 @@ void proxy_shutdown_forwarder(struct xnbd_proxy *proxy)
 /* called in a proxy process */
 void proxy_initialize(struct xnbd_info *xnbd, struct xnbd_proxy *proxy)
 {
-#ifdef NEED_GTHREAD_INIT
 	g_thread_init(NULL);
-#endif
 
 
 	proxy->xnbd  = xnbd;
